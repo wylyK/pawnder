4edd5f7d10c477e56d9f808fa62b328afa3e4a54
Châu Anh Phạm <phamchauanh2509@gmail.com>
2024-10-27 14:31:58 -0400

    User Login/Logout

--- backend/app.py
+++ backend/app.py
@@ -4,9 +4,11 @@ Entrypoint to Flask backend server
 
 from flask import Flask
 from api.users import users_api
+import os
 
 app = Flask(__name__)
 app.register_blueprint(users_api)
+app.secret_key = os.getenv('SECRET_KEY')
 
 @app.route("/")
 def hello_world():
