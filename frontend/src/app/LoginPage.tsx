410ee0b6c72862a1e39e5a1750f6ca35e48b2fcb
wylyK <willychen720360@gmail.com>
2024-11-14 12:22:22 -0500

    declare global css variables, tweak login UI

--- frontend/src/app/LoginPage.tsx
+++ frontend/src/app/LoginPage.tsx
@@ -6,26 +6,29 @@ import { FaPaw } from "react-icons/fa";
 
 const LoginPage: React.FC = () => {
     return (
-        <div className={styles.Login}>
-            <div className={styles.logo}>
-                <FaPaw className={styles.logoIcon}/><span className={styles.logoText}>Pawnder</span>
+        <div className={styles.loginContainer}>
+            <div className={styles.login}>
+                <div className={styles.logo}>
+                    <FaPaw className={styles.logoIcon}/><span className={styles.logoText}>Pawnder</span>
+                </div>
+                <form>
+                    <h1>Login</h1>
+                    <div className={styles.textField} >
+                        <FaUserLarge className={styles.textIcon}/>
+                        <input type="email" placeholder="Email"></input>
+                    </div>
+                    <div className={styles.textField} >
+                        <FaLock className={styles.textIcon}/>
+                        <input type="password" placeholder="Password"></input>
+                    </div>
+                    <div className={styles.rememberMe}>
+                            <input type="checkbox"/>
+                            <div className={styles.rememberText}>Remember me</div>
+                    </div>
+                    <input className={styles.submit} type="submit"></input>
+                    <button className={styles.signUp}>Create an Account</button>
+                </form>
             </div>
-            <form>
-                <h1>Login</h1>
-                <div className={styles.emailField} >
-                    <FaUserLarge className={styles.userIcon}/>
-                    <input type="email" placeholder="Email"></input>
-                </div>
-                <div className={styles.passwordField} >
-                    <FaLock className={styles.lockIcon}/>
-                    <input type="password" placeholder="Password"></input>
-                </div>
-                <div className={styles.rememberMe}>
-                        <input type="checkbox"/><div className={styles.rememberText}>Remember me</div>
-                </div>
-                <input className={styles.submit} type="submit"></input>
-                <button className={styles.signUp}>Create an Account</button>
-            </form>
         </div>
     )
 }
--- frontend/src/components/Reminder/Reminders.tsx
+++ frontend/src/components/Reminder/Reminders.tsx
@@ -3,20 +3,23 @@ import styles from "./Reminders.module.css"
 import { AiOutlinePlus } from "react-icons/ai";
 
 interface Reminder {
-    time:  string;
-    text:  string;
+    pet : string;
+    time: string;
+    body: string;
     fromVet: boolean;
 }
 
 const reminders: Reminder[] = [
     {
+      pet: "Fionn",
       time: "15:10",
-      text: "Go for a walk",
+      body: "Go for a walk",
       fromVet: false,
     },
     {
+      pet : "Spike",
       time: "16:30",
-      text: "Feed medicine",
+      body: "Feed medicine",
       fromVet: true,
     },
   ];
@@ -29,11 +32,12 @@ const Reminders = () => {
             </div>
             {reminders.map((reminder, index) => (
                 <div key={index} className={styles.reminder}>
-                    <h3 className={styles.reminderHead}>{reminder.text}</h3>
+                    <h3 className={styles.reminderHead}>{reminder.pet}</h3>
                     <div className={styles.reminderSubHead}>
                         <p className={styles.reminderTime}>{reminder.time}</p>
                         <p><i>{reminder.fromVet && " from Dr. Willy"}</i></p>
                     </div>
+                    <p className={styles.reminderBody}>{reminder.body}</p>
                     <div className={styles.reminderButtons}>
                         <button>Edit</button>
                         <button>Done</button>
