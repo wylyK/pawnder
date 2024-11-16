2b683a0939bb8b1a2ecd0d8e1ac1791d614db14a
wylyK <willychen720360@gmail.com>
2024-11-16 03:52:02 -0500

    switch to kebab case and tweak login and reminder UI

--- frontend/src/app/LoginPage.tsx
+++ frontend/src/app/LoginPage.tsx
@@ -6,28 +6,28 @@ import { FaPaw } from "react-icons/fa";
 
 const LoginPage: React.FC = () => {
   return (
-    <div className={styles.loginContainer}>
-      <div className={styles.login}>
-        <div className={styles.logo}>
-          <FaPaw className={styles.logoIcon} />
-          <span className={styles.logoText}>Pawnder</span>
+    <div className={styles["login-container"]}>
+      <div className={styles["login"]}>
+        <div className={styles["logo"]}>
+          <FaPaw className={styles["logo-icon"]} />
+          <span className={styles["logo-text"]}>Pawnder</span>
         </div>
         <form>
           <h1>Login</h1>
-          <div className={styles.textField}>
-            <FaUserLarge className={styles.textIcon} />
+          <div className={styles["text-field"]}>
+            <FaUserLarge className={styles["text-icon"]} />
             <input type="email" placeholder="Email"></input>
           </div>
-          <div className={styles.textField}>
-            <FaLock className={styles.textIcon} />
+          <div className={styles["text-field"]}>
+            <FaLock className={styles["text-icon"]} />
             <input type="password" placeholder="Password"></input>
           </div>
-          <div className={styles.rememberMe}>
+          <div className={styles["remember-me"]}>
             <input type="checkbox" />
-            <div className={styles.rememberText}>Remember me</div>
+            <div className={styles["remember-text"]}>Remember me</div>
           </div>
-          <input className={styles.submit} type="submit"></input>
-          <button className={styles.signUp}>Create an Account</button>
+          <input className={styles["submit"]} type="submit"></input>
+          <button className={styles["sign-up"]}>Create an Account</button>
         </form>
       </div>
     </div>
--- frontend/src/components/Reminder/Reminders.tsx
+++ frontend/src/components/Reminder/Reminders.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+'use client';
+
+import React, { useState } from "react";
 import styles from "./Reminders.module.css";
-import { AiOutlinePlus } from "react-icons/ai";
+import { AiOutlinePlusCircle, AiFillPlusCircle } from "react-icons/ai";
+import { PiTrash, PiTrashFill } from "react-icons/pi";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { FaClock,
+         FaRegCheckCircle,
+         FaCheckCircle} from "react-icons/fa"
+
 
 interface Reminder {
   pet: string;
@@ -9,40 +17,63 @@ interface Reminder {
   fromVet: boolean;
 }
 
-const reminders: Reminder[] = [
-  {
-    pet: "Fionn",
-    time: "15:10",
-    body: "Go for a walk",
-    fromVet: false,
-  },
-  {
-    pet: "Spike",
-    time: "16:30",
-    body: "Feed medicine",
-    fromVet: true,
-  },
-];
+const walk: Reminder = {
+  pet: "Fionn",
+  time: "15:10",
+  body: "Go for a walk",
+  fromVet: false
+};
+
+const feed: Reminder = {
+  pet: "Spike",
+  time: "16:30",
+  body: "Feed medicine",
+  fromVet: true,
+}
+
+const wash: Reminder = {
+  pet: "Bella",
+  time: "17:30",
+  body: "Use water and put it on Bella, make sure its warm",
+  fromVet: false,
+}
 
 const Reminders = () => {
+  const [reminders, updateReminders] = useState<Reminder[]>([walk, feed, wash]);
+
   return (
-    <div className={styles.reminders}>
-      <div className={styles.addReminder}>
-        <AiOutlinePlus />
+    <div className={styles["reminders"]}>
+      <div className={styles["add-delete"]}>
+        <div className={styles["add-delete-container"]}>
+          <AiOutlinePlusCircle className={styles["dormant"]}/>
+          <AiFillPlusCircle className={styles["hovered"]}/>
+        </div>
+        <div className={styles["add-delete-container"]}>
+          <PiTrash className={styles["dormant"]}/>
+          <PiTrashFill className={styles["hovered"]}/>
+        </div>
       </div>
       {reminders.map((reminder, index) => (
-        <div key={index} className={styles.reminder}>
-          <h3 className={styles.reminderHead}>{reminder.pet}</h3>
-          <div className={styles.reminderSubHead}>
-            <p className={styles.reminderTime}>{reminder.time}</p>
-            <p>
-              <i>{reminder.fromVet && " from Dr. Willy"}</i>
-            </p>
+        <div key={index} className={styles["reminder"]}>
+          <div className={styles["main-content"]}>
+            <div className={styles["heading"]}>
+              <div className="clock"><FaClock/></div>
+              <div className={styles["name"]}>{reminder.pet}</div>
+            </div>
+            <div className={styles["subhead"]}>
+              <p className={styles["time"]}>{reminder.time}</p>
+              <p>
+                <i>{reminder.fromVet && " from Dr. Willy"}</i>
+              </p>
+            </div>
+            <p className={styles["body"]}>{reminder.body}</p>
           </div>
-          <p className={styles.reminderBody}>{reminder.body}</p>
-          <div className={styles.reminderButtons}>
-            <button>Edit</button>
-            <button>Done</button>
+          <div className={styles["edit-check"]}>
+            <button className={styles["edit"]}><BsThreeDotsVertical/></button>
+            <div className={styles["check-container"]} onClick={() => {updateReminders(reminders.slice(index))}}>
+              <FaRegCheckCircle className={styles["dormant"]} />
+              <FaCheckCircle className={styles["hovered"]}/>
+            </div>
           </div>
         </div>
       ))}
