26533efb613602a5ae751cfb07ebb09d27549bb1
sowrathi <ssomasundara@umass.edu>
2024-12-08 22:27:18 -0500

    updated the model with presc and insurance

--- backend/models/health.py
+++ backend/models/health.py
@@ -1,19 +1,21 @@
 class Health:
-    def __init__(self, VetId = [], Weight = None, Height = None, Diet="", Medicine=[]):
+    def __init__(self, VetId = "", Weight = None, Height = None, Diet="", Prescription="", Insurance = ""):
         self.VetId = VetId
         self.Weight = Weight
         self.Height = Height
         self.Diet = Diet
-        self.Medicine = Medicine
+        self.Prescription = Prescription
+        self.Insurance = Insurance
 
     @staticmethod
     def from_dict(source):
         health = Health(
-            source.get('VetId', []),
+            source.get('VetId', ""),
             source.get('Weight', None),
             source.get('Height', None),
             source.get('Diet', ""),
-            source.get('Medicine', [])
+            source.get('Prescription', ""),
+            source.get('Insurance', "")
         )
         return health
     
@@ -23,11 +25,12 @@ class Health:
             'Weight': self.Weight,
             'Height': self.Height,
             'Diet': self.Diet,
-            'Medicine': self.Medicine
+            'Prescription': self.Prescription,
+            'Insurance' : self.Insurance
         }
         return health
     
     def __repr__(self):
         return(
-            f"Health(VetId={self.VetId}, Weight={self.Weight}, Height={self.Height}, Diet={self.Diet}, Medicine={self.Medicine})"
+            f"Health(VetId={self.VetId}, Weight={self.Weight}, Height={self.Height}, Diet={self.Diet}, Insurance={self.Insurance}, Prescription={self.Prescription} )"
         )
