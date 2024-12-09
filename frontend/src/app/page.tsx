5452678b328d0ee27a942c375c4b3fd995d87cf3
sowrathi <ssomasundara@umass.edu>
2024-12-08 22:29:06 -0500

    updated redirecting vets to vetHomePage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,33 @@
 // app/page.tsx
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import HomePage from "../components/Home/Home";
+import VetHomePage from "../components/VetPage/VetHomePage"; // Import VetHomePage
+import { useAuth } from "@/context/UserContext"; // Import authentication context
+import { useRouter } from "next/navigation";
 import "./globals.css";
 
 const Page: React.FC = () => {
-  return <HomePage />;
+  const { user } = useAuth(); // Get user details from context
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user) {
+      router.push("/login"); // Redirect to login if no user is logged in
+    }
+  }, [user, router]);
+
+  if (!user) {
+    return <div>Loading...</div>; // Show a loading state while waiting for authentication
+  }
+
+  return (
+    <>
+      {user?.Role === "Owner" && <HomePage />}
+      {user?.Role === "Vet" && <VetHomePage />}
+    </>
+  );
 };
 
 export default Page;
